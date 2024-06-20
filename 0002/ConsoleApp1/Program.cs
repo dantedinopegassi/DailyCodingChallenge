746943b8ea77bf9a2fddd910bd123db7de917394
dinopegassi <nmbost@gmail.com>
2024-06-21 08:04:22 +0930

    [0002] Solved - i wont do the 'no division' part just yet

---
+++ 0002/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+/********************************************************************|
+| Given an array of integers, return a new array such that each      |
+| element at index i of the new array is the product of all the      |
+| numbers in the original array except the one at i.                 |
+|                                                                    |
+| For example, if our input was [1, 2, 3, 4, 5], the expected        | 
+| output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], |
+| the expected output would be [2, 3, 6].                            |
+|                                                                    |
+| Follow-up: what if you can't use division?                         |
+|********************************************************************/
+
+// list = [6,7,8,9,11,12,14,15,10]
+
+long[] FactorialArrayDupe(int[] list)
+{
+    long[] thing = new long[list.Length];
+    long total = 1;
+    for (int i = 0; i < list.Length; i++)
+    {
+        total *= list[i];
+    }
+    for (int i = 0; i < list.Length; i++)
+    {
+        thing[i] = total/list[i];
+    }
+    return thing;
+}
+
+long[] result = FactorialArrayDupe([6,7,8,9,11,12,14,15,10]);
+StringBuilder stringcito = new StringBuilder();
+foreach(var item in result){
+    stringcito.Append("[" + item.ToString() + "] ");
+}
+
+Console.WriteLine(stringcito);
